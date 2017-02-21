adda045184a443fdeb8ffa18922b924c09dc9df5
轻色年华 <13675626825@163.com>
2017-02-21 19:49:12 +0800

    base

--- app/Http/Controllers/Api/UserController.php
+++ app/Http/Controllers/Api/UserController.php
@@ -29,8 +29,8 @@ class UserController extends Controller
         }
 
         $user = User::create([
-            'name' => $request->input('name'),
-            'email' => $request->input('email'),
+            'name'     => $request->input('name'),
+            'email'    => $request->input('email'),
             'password' => bcrypt($request->input('password')),
         ]);
 
--- routes/api.php
+++ routes/api.php
@@ -16,17 +16,28 @@ Route::post('v1/user/add', 'Api\UserController@create');
 
 Route::group(['prefix' => 'v1', 'middleware' => ['auth:api']], function () {
 
+    /** 获取用户信息 */
     Route::get('/user', 'Api\UserController@me');
+    /** 更换密码 */
     Route::post('/user/change_password', 'Api\UserController@postChangePassword');
 
+    /** 获取所有分类 */
     Route::get('/category', 'Api\CategoryController@all');
+    /** 添加分类 */
     Route::post('/category/add', 'Api\CategoryController@create');
+    /** 查找分类 */
     Route::get('/category/{id}/find', 'Api\CategoryController@find');
+    /** 编辑分类 */
     Route::post('/category/{id}/edit', 'Api\CategoryController@edit');
+    /** 删除分类 */
     Route::delete('/category/{id}/delete', 'Api\CategoryController@delete');
 
+    /** 获取所有书签 */
     Route::get('/bookmark', 'Api\BookmarkController@all');
+    /** 添加书签 */
     Route::post('/bookmark/add', 'Api\BookmarkController@create');
+    /** 编辑书签 */
     Route::post('/bookmark/{id}/edit', 'Api\BookmarkController@edit');
+    /** 删除书签 */
     Route::delete('/bookmark/{id}/delete', 'Api\BookmarkController@delete');
 });
