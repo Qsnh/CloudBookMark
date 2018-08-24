80e054c555ace6439d9239bba7aa38aa4338984c
XiaoTeng <13675626825@163.com>
2018-08-24 14:33:07 +0800

    update frontend dependency

--- resources/assets/js/app.js
+++ resources/assets/js/app.js
@@ -1,20 +1,12 @@
+require('./bootstrap')
 
-/**
- * First we will load all of this project's JavaScript dependencies which
- * includes Vue and other libraries. It is a great starting point when
- * building robust, powerful web applications using Vue and Laravel.
- */
+window.Vue = require('vue')
 
-require('./bootstrap');
+import ElementUI from 'element-ui'
+import 'element-ui/lib/theme-chalk/index.css'
+import mavonEditor from 'mavon-editor'
+import 'mavon-editor/dist/css/index.css'
 
-/**
- * Next, we will create a fresh Vue application instance and attach it to
- * the page. Then, you may begin adding components to this application
- * or customize the JavaScript scaffolding to fit your unique needs.
- */
 
-// Vue.component('example', require('./components/Example.vue'));
-
-const app = new Vue({
-    el: '#app'
-});
+Vue.use(ElementUI)
+Vue.use(mavonEditor)
